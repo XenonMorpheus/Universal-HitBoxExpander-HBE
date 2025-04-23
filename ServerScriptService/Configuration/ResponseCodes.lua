2c4c9d6f434f20aae9a38a299f2b05c392206ea8
Van Rave <barangganvanrave@gmail.com>
2025-04-23 12:55:02 +0800

    Create ResponseCodes.lua

---
+++ ServerScriptService/Configuration/ResponseCodes.lua
@@ -0,0 +1,7 @@
+-- ReplicatedStorage/ResponseCodes.lua
+return {
+    MT01 = "Memory tampering detected",
+    SD01 = "Abnormal hitbox size detected",
+    PV01 = "Illegal property modification",
+    CG01 = "Collision group violation"
+}
